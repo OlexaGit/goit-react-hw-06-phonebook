4876669c0a3ea267976ac4025a8dcb52d6d1db8a
OlexaGit <109541565+OlexaGit@users.noreply.github.com>
2023-07-02 13:44:07 +0300

    update App on redux

--- src/components/Contacts/Contacts.js
+++ src/components/Contacts/Contacts.js
@@ -1,30 +1,28 @@
 import css from './Contacts.module.css';
-import { increment, contacts, filter } from 'redux/store';
-import { useSelector, useDispatch } from 'react-redux';
+// import { increment, contacts, filter } from 'redux/store';
+// import { useSelector, useDispatch } from 'react-redux';
 
-const Contacts = ({ contacts, onDeleteContact }) => {
-  const dispatch = useDispatch;
-  const value = useSelector(state => state.myValue);
+export const Contacts = () => {
+  // const dispatch = useDispatch;
+  // const value = useSelector(state => state.myValue);
+
+  const contacts = [];
   return (
     <div className={css.form}>
-      {value}
-      <button onClick={() => dispatch(increment(1))}>increment</button>
       <ul>
         {contacts.map(({ id, name, number }) => (
           <li key={id} className={css.formList}>
             @ {name}: {number}
-            <button
+            {/* <button
               className={css.formButton}
               type="button"
               onClick={() => onDeleteContact(id)}
             >
               Delete
-            </button>
+            </button> */}
           </li>
         ))}
       </ul>
     </div>
   );
 };
-
-export default Contacts;
