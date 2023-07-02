9175d4f9463e0a7b34d9a2abb0d84df811bb005d
OlexaGit <109541565+OlexaGit@users.noreply.github.com>
2023-07-02 14:16:02 +0300

    added persist

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,71 +1,71 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { createAction, createReducer } from '@reduxjs/toolkit';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import { PersistGate } from 'redux-persist/integration/react';
 
-const initialState = { contacts: [] };
+import contactsReducer from './contactSlice';
 
-export const increment = createAction('myValue/increment');
+const persistConfig = {
+  key: 'contact',
+  storage,
+};
 
-export const filter = createAction('valueFilter/filter');
-
-const myReducerTest = createReducer(100, {
-  [increment]: (state, action) => state + action.payload,
-});
-
-export const addContact = createAction('Contacts/addContact');
-export const deleteContact = createAction('Contacts/deleteContact');
-const contactsReducer = createReducer([], {
-  [addContact]: (state, { payload: newContact }) => [...state, newContact],
-  [deleteContact]: (state, { payload: removeId }) => {
-    state.contacts = state.contacts.filter(({ id }) => id !== removeId);
-  },
-});
-const filterReducer = createReducer('', {
-  [filter]: (state, action) => state + action.payload,
-});
+const persistedReducer = persistReducer(persistConfig, contactsReducer);
 
 export const store = configureStore({
-  reducer: {
-    myValue: myReducerTest,
-    arrayContacts: contactsReducer,
-    valueFilter: filterReducer,
-  },
+  reducer: persistedReducer,
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+
+export let persistor = persistStore(store);
+
+// import { createAction, createReducer } from '@reduxjs/toolkit';
+
+// const initialState = { contacts: [] };
+
+// export const increment = createAction('myValue/increment');
+
+// export const filter = createAction('valueFilter/filter');
+
+// const myReducerTest = createReducer(100, {
+//   [increment]: (state, action) => state + action.payload,
+// });
+
+// export const addContact = createAction('Contacts/addContact');
+// export const deleteContact = createAction('Contacts/deleteContact');
+// const contactsReducer = createReducer([], {
+//   [addContact]: (state, { payload: newContact }) => [...state, newContact],
+//   [deleteContact]: (state, { payload: removeId }) => {
+//     state.contacts = state.contacts.filter(({ id }) => id !== removeId);
+//   },
+// });
+// const filterReducer = createReducer('', {
+//   [filter]: (state, action) => state + action.payload,
+// });
+
+// export const store = configureStore({
+//   reducer: {
+//     myValue: myReducerTest,
+//     arrayContacts: contactsReducer,
+//     valueFilter: filterReducer,
+//   },
+// });
 
 // {
 //   contacts: [],
 //   filter: ""
 // }
-
-// import {
-//   persistStore,
-//   persistReducer,
-//   FLUSH,
-//   REHYDRATE,
-//   PAUSE,
-//   PERSIST,
-//   PURGE,
-//   REGISTER,
-// } from 'redux-persist';
-// import storage from 'redux-persist/lib/storage';
-// import { PersistGate } from 'redux-persist/integration/react';
-
-// import toDosReducer from './toDosSlice';
-
-// const persistConfig = {
-//   key: 'todos',
-//   storage,
-// };
-
-// const persistedReducer = persistReducer(persistConfig, toDosReducer);
-
-// export const store = configureStore({
-//   reducer: persistedReducer,
-//   middleware: getDefaultMiddleware =>
-//     getDefaultMiddleware({
-//       serializableCheck: {
-//         ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-//       },
-//     }),
-// });
-
-// export let persistor = persistStore(store);
